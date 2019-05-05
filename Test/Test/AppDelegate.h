1deb842d256c88b2b6dbc19b50afb458799a1936
Adam Evans <illusion90@me.com>
2019-05-04 22:18:25 -0500

    Add the leaky app

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Adam Evans on 5/4/19.
+//  Copyright © 2019 Adam Evans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Adam Evans on 5/4/19.
+//  Copyright © 2019 Adam Evans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
